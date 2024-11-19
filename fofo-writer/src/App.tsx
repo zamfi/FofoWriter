8db73cfb35e91f445f854d68fa5bacadeda3fdfe
s-almeda <shm.almeda@berkeley.edu>
2024-11-19 14:21:57 -0800

    change it so the website actually shows up

--- fofo-writer/src/App.tsx
+++ fofo-writer/src/App.tsx
@@ -1,8 +1,8 @@
 import ScriptCoWriter from './script-cowriter'
 
 function App() {
-  return <div> Hello React</div>
-  //return <ScriptCoWriter />
+  //return <div> Hello React</div>
+  return <ScriptCoWriter />
 }
 
 export default App
