093b7225809034e4f8536849af763f7599c3a8b6
J.D. Zamfirescu-Pereira <zamfire@gmail.com>
2024-12-07 23:08:59 -0800

    port from env

--- fofo-writer/server/server.js
+++ fofo-writer/server/server.js
@@ -74,7 +74,7 @@ const server = http.createServer(async (req, res) => {
   console.error(err);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
